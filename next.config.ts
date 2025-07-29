13e838fa5b5aeedb9a5230c92bb880e3f95f6c0c
sumankumargit <sumankumarchoudhary733@gmail.com>
2025-07-30 03:45:36 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "www.mhebazar.in",
+      },
+      {
+        protocol: "https",
         hostname: "images.unsplash.com",
       },
       {
